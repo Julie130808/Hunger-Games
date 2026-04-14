e63af833b31fc8f55f412d5ae77d21aacb60b2ec
JulieCass1308 <julie0510@live.fr>
2026-04-14 16:55:16 +0200

    version MCV terminée

--- views/admin/users/utilisateurs.php
+++ views/admin/users/utilisateurs.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . '/../../../includes/header.php'; ?>
+<?php include __DIR__ . '/../includes/header.php'; ?>
 
 <div class="utilisateurs">
 
@@ -34,4 +34,4 @@
 
 </div>
 
-<?php include __DIR__ . '/../../../includes/footer.php'; ?>
+<?php include __DIR__ . '/../includes/footer.php'; ?>
